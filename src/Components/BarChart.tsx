bd33ff13383d80e7e429c0d95c3dbfdf717b7bba
tommyrharper <thomasrobertharper@gmail.com>
2021-03-14 04:45:02 +0000

    Fix BarChart.scss

--- src/Components/BarChart.tsx
+++ src/Components/BarChart.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-// import styles from './BarChart.scss';
-// import * as styles from './BarChart.scss';
+import "./BarChart.scss";
 
-
-const ARR_LENGTH = 5
+const ARR_LENGTH = 5;
 
 const generateArray = (length: number): number[] => {
   const arr = [];
@@ -17,21 +15,24 @@ const BarChart: React.FC = () => {
   const [array, setArray] = useState<number[]>(() => generateArray(ARR_LENGTH));
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-end'}}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-end",
+      }}
+    >
       {console.log(`array`, array)}
-      {array.map((x) => {
+      {array.map((x, i) => {
         console.log(`x`, x);
         const height = x * 50;
         console.log(`height`, height);
         return (
-          <div 
-            // className={styles.bar}
+          <div
+            key={`${x}${i}`}
+            className="bar"
             style={{
               height: `${height}px`,
-              width: "50px",
-              padding: "10px",
-              margin: "10px",
-              border: "1px solid black",
             }}
           >
             {x}
