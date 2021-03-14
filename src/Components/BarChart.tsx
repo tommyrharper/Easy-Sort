615882676e14cb2a202f5e0607221a4a2fc44e9a
tommyrharper <thomasrobertharper@gmail.com>
2021-03-14 05:22:45 +0000

    Add bubbleSort

--- src/Components/BarChart.tsx
+++ src/Components/BarChart.tsx
@@ -12,8 +12,28 @@ const generateArray = (length: number): number[] => {
   return arr;
 };
 
+const bubbleSort = (array: number[]): number[] => {
+  const arr = [...array];
+  let changeMade = true;
+  while (changeMade) {
+    changeMade = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      const bar1 = arr[i];
+      const bar2 = arr[i + 1]
+      if (bar1 > bar2) {
+        arr[i] = bar2
+        arr[i + 1] = bar1;
+        changeMade = true;
+      }
+    }
+  }
+  return arr;
+}
+
 const BarChart: React.FC = () => {
   const [array, setArray] = useState<number[]>(() => generateArray(ARR_LENGTH));
+
+  console.log(`bubbleSort(array)`, bubbleSort(array));
 
   return (
     <div className="barsContainer">
@@ -24,6 +44,7 @@ const BarChart: React.FC = () => {
         console.log(`height`, height);
         return (
           <Box
+            key={`${x}${i}`}
             className="bar"
             height={`${height}px`}
             borderRadius="lg"
