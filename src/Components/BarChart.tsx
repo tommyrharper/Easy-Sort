2fc0e6a9571a19b7b7b9bedc0c0df347ae319e46
tommyrharper <thomasrobertharper@gmail.com>
2021-03-14 06:09:03 +0000

    Add working bubblesort button

--- src/Components/BarChart.tsx
+++ src/Components/BarChart.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import "./BarChart.scss";
-import { Box } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 const ARR_LENGTH = 5;
 
 const generateArray = (length: number): number[] => {
-  const arr = [];
-  for (let i = 0; i < length; i++) {
-    arr.push(Math.floor(Math.random() * length) + 1);
+  let arr = [];
+  let sorted = true;
+  while (sorted) {
+    arr = [];
+    for (let i = 0; i < length; i++) {
+      arr.push(Math.floor(Math.random() * length) + 1);
+      if (!i) continue;
+      if (arr[i] < arr[i - 1]) sorted = false;
+    }
   }
   return arr;
 };
@@ -19,16 +25,16 @@ const bubbleSort = (array: number[]): number[] => {
     changeMade = false;
     for (let i = 0; i < arr.length - 1; i++) {
       const bar1 = arr[i];
-      const bar2 = arr[i + 1]
+      const bar2 = arr[i + 1];
       if (bar1 > bar2) {
-        arr[i] = bar2
+        arr[i] = bar2;
         arr[i + 1] = bar1;
         changeMade = true;
       }
     }
   }
   return arr;
-}
+};
 
 const BarChart: React.FC = () => {
   const [array, setArray] = useState<number[]>(() => generateArray(ARR_LENGTH));
@@ -36,24 +42,24 @@ const BarChart: React.FC = () => {
   console.log(`bubbleSort(array)`, bubbleSort(array));
 
   return (
-    <div className="barsContainer">
-      {console.log(`array`, array)}
-      {array.map((x, i) => {
-        console.log(`x`, x);
-        const height = x * 50;
-        console.log(`height`, height);
-        return (
+    <div>
+      <div className="barsContainer">
+        {console.log(`array`, array)}
+        {array.map((x, i) => (
           <Box
             key={`${x}${i}`}
             className="bar"
-            height={`${height}px`}
+            height={`${x * 50}px`}
             borderRadius="lg"
             backgroundColor="teal.300"
           >
             {x}
           </Box>
-        );
-      })}
+        ))}
+      </div>
+      <Button marginTop="40px" onClick={() => setArray(a => bubbleSort(a))}>
+        Sort
+      </Button>
     </div>
   );
 };
