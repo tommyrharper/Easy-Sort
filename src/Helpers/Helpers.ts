e484044e63ac84d8d204395deb6b9831133a0e6e
tommyrharper <thomasrobertharper@gmail.com>
2021-03-15 00:52:00 +0000

    Extract generate array to Helpers.ts

---
+++ src/Helpers/Helpers.ts
@@ -0,0 +1,14 @@
+export const generateArray = (length: number): number[] => {
+  let arr = [];
+  let sorted = true;
+  while (sorted) {
+    arr = [];
+    for (let i = 0; i < length; i++) {
+      arr.push(Math.floor(Math.random() * length) + 1);
+    }
+    for (let i = 0; i < length - 1; i++) {
+      if (arr[i] > arr[i + 1]) sorted = false;
+    }
+  }
+  return arr;
+};
