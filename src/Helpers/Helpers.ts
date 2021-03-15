6676f8b8433bbc0b4f1598b4a81ea9970b6b2344
tommyrharper <thomasrobertharper@gmail.com>
2021-03-15 02:44:04 +0000

    Add bubbleSort to Helper.ts

--- src/Helpers/Helpers.ts
+++ src/Helpers/Helpers.ts
@@ -12,3 +12,27 @@ export const generateArray = (length: number): number[] => {
   }
   return arr;
 };
+
+export const sleep = (milliseconds: number): void => {
+  var currentTime = new Date().getTime();
+
+  while (currentTime + milliseconds >= new Date().getTime()) {}
+};
+
+export const bubbleSort = (array: number[]): number[] => {
+  const arr = [...array];
+  let changeMade = true;
+  while (changeMade) {
+    changeMade = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      const bar1 = arr[i];
+      const bar2 = arr[i + 1];
+      if (bar1 > bar2) {
+        arr[i] = bar2;
+        arr[i + 1] = bar1;
+        changeMade = true;
+      }
+    }
+  }
+  return arr;
+};
