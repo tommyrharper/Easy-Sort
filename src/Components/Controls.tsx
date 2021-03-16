68c3d2b5ee481d0fab8b032767f5d8a2b213137f
tommyrharper <thomasrobertharper@gmail.com>
2021-03-16 02:16:42 +0000

    Add Speed slider to the Controls.tsx

--- src/Components/Controls.tsx
+++ src/Components/Controls.tsx
@@ -1,5 +1,14 @@
 import React from "react";
-import { Select, FormLabel, Box } from "@chakra-ui/react";
+import {
+  Select,
+  FormLabel,
+  Box,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from "@chakra-ui/react";
+import { calcSpeed } from "../Helpers/Helpers";
 
 interface Person {
   firstName: string;
@@ -17,17 +26,31 @@ interface Props {
 }
 
 const Controls: React.FC<Props> = ({ values, handleChange }) => {
-  console.log(`values`, values);
-  // console.log(`algo`, algo);
   return (
     <Box>
       <FormLabel>Select Sorting Algorithm:</FormLabel>
-      <Select value={values.algo} onChange={handleChange} name="algo">
+      <Select
+        value={values.algo}
+        onChange={handleChange}
+        name="algo"
+        marginBottom="20px"
+      >
         <option value="Bubble Sort">Bubble Sort</option>
         <option value="Insertion Sort">Insertion Sort</option>
         <option value="Selection Sort">Selection Sort</option>
       </Select>
       <FormLabel>Select Speed:</FormLabel>
+      <Slider
+        defaultValue={calcSpeed(values.delay)}
+        onChange={handleChange}
+        colorScheme="teal"
+        name="speed"
+      >
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
     </Box>
   );
 };
