4d5fb8f07317c5c529244919f259d3f7a5f56cf4
tommyrharper <thomasrobertharper@gmail.com>
2021-03-16 01:18:21 +0000

    Remove bubbleSort from Helpers.ts

--- src/Helpers/Helpers.ts
+++ src/Helpers/Helpers.ts
@@ -18,21 +18,3 @@ export const sleep = (milliseconds: number): void => {
 
   while (currentTime + milliseconds >= new Date().getTime()) {}
 };
-
-export const bubbleSort = (array: number[]): number[] => {
-  const arr = [...array];
-  let changeMade = true;
-  while (changeMade) {
-    changeMade = false;
-    for (let i = 0; i < arr.length - 1; i++) {
-      const bar1 = arr[i];
-      const bar2 = arr[i + 1];
-      if (bar1 > bar2) {
-        arr[i] = bar2;
-        arr[i + 1] = bar1;
-        changeMade = true;
-      }
-    }
-  }
-  return arr;
-};
