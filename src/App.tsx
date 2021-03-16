7fda55844e7cf32433bb263317fc5b7c4aeef571
tommyrharper <thomasrobertharper@gmail.com>
2021-03-16 00:58:25 +0000

    Add dropdown to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
-import './App.scss';
+import React, { useState } from "react";
+import "./App.scss";
+import { useForm } from "./Hooks/useForm";
 
-import {
-  Box,
-  Heading,
-} from "@chakra-ui/react"
-import BarChart from './Components/BarChart';
+import { Box, Heading, Select } from "@chakra-ui/react";
+import BarChart from "./Components/BarChart";
 
 export const App: React.FC = () => {
+  const [values, handleChange] = useForm({
+    algo: "Bubble Sort",
+  });
+
   return (
     <Box className="appContainer" fontSize="xl">
-      <Heading className="heading" size="2xl" color="teal.300">Easy-Sort</Heading>
-      <BarChart/>
+      <Heading className="heading" size="2xl" color="teal.300">
+        Easy-Sort
+      </Heading>
+      <Box marginBottom="20px">
+        <Select value={values.algo} onChange={handleChange} name="algo">
+          <option value="Bubble Sort">Bubble Sort</option>
+          <option value="Insertion Sort">Insertion Sort</option>
+          <option value="Selection Sort">Selection Sort</option>
+        </Select>
+      </Box>
+      <BarChart />
     </Box>
   );
-}
-
+};
