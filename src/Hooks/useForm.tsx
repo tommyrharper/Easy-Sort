53e76ed6eeba06d598c78f0808ad72137e979644
tommyrharper <thomasrobertharper@gmail.com>
2021-03-16 02:17:14 +0000

    Allow useForm to handle numbers

--- src/Hooks/useForm.tsx
+++ src/Hooks/useForm.tsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
+import { calcDelay } from "../Helpers/Helpers";
 
 export const useForm = (initialValues: any) => {
   const [values, setValues] = useState(initialValues);
-
   return [
     values,
     (e: React.ChangeEvent<HTMLSelectElement>) => {
+      let name: string;
+      let value: string | number;
+      if (typeof e === "number") {
+        name = "delay";
+        value = calcDelay(parseInt(e));
+      } else {
+        name = e.target.name;
+        value = e.target.value;
+      }
       setValues({
         ...values,
-        [e.target.name]: e.target.value,
+        [name]: value,
       });
     },
   ];
