886c161e282d3f9a09c6f08ac42f4ea2c603008d
tommyrharper <thomasrobertharper@gmail.com>
2021-03-16 01:18:01 +0000

    Extract bubbleSort from BarChart.tsx

---
+++ src/Algos/bubbleSort.tsx
@@ -0,0 +1,72 @@
+interface Animation {
+  startValues: number[];
+  endValues: number[];
+  positions: number[];
+}
+
+export const getBubbleSortAnimations = (array: number[]): Animation[] => {
+  const arr = [...array];
+  const animations = [];
+  let changeMade = true;
+  while (changeMade) {
+    changeMade = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      const bar1 = arr[i];
+      const bar2 = arr[i + 1];
+      if (bar1 > bar2) {
+        arr[i] = bar2;
+        arr[i + 1] = bar1;
+        changeMade = true;
+      }
+      const animation: Animation = {
+        startValues: [bar1, bar2],
+        endValues: [arr[i], arr[i + 1]],
+        positions: [i, i + 1],
+      };
+      animation.endValues = [arr[i], arr[i + 1]];
+      animations.push(animation);
+    }
+  }
+  return animations;
+};
+
+export const bubbleSort = (
+  array: number[],
+  delay: number,
+  calcHeight: (h: number) => number
+) => {
+  const animations = getBubbleSortAnimations(array);
+  const arrayBars = document.getElementsByClassName(
+    "bar"
+  ) as HTMLCollectionOf<HTMLElement>;
+  for (let i = 0; i < animations.length; i++) {
+    const { startValues, endValues, positions } = animations[i];
+    const pos1 = positions[0];
+    const pos2 = positions[1];
+    const height1 = `${calcHeight(endValues[0])}px`;
+    const height2 = `${calcHeight(endValues[1])}px`;
+    setTimeout(() => {
+      for (let j = 0; j < arrayBars.length; j++) {
+        if (j !== pos1 && j !== pos2) {
+          arrayBars[j].style.backgroundColor = "#4FD1C5";
+        }
+      }
+      arrayBars[pos1].style.backgroundColor = "red";
+      arrayBars[pos2].style.backgroundColor = "red";
+      setTimeout(() => {
+        arrayBars[pos1].style.height = height1;
+        arrayBars[pos2].style.height = height2;
+        arrayBars[pos1].innerHTML = `${endValues[0]}`;
+        arrayBars[pos2].innerHTML = `${endValues[1]}`;
+      }, Math.floor(delay / 3));
+      if (i === animations.length - 1) {
+        // clear all bars
+        setTimeout(() => {
+          for (let j = 0; j < arrayBars.length; j++) {
+            arrayBars[j].style.backgroundColor = "#4FD1C5";
+          }
+        }, delay);
+      }
+    }, i * delay);
+  }
+};
