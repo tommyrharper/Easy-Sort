5eed118f6dad39c5cbac8c15f34703b4f2bf3dea
tommyrharper <thomasrobertharper@gmail.com>
2021-03-16 22:01:18 +0000

    Extract handle numbers method from useForm.tsx

--- src/Hooks/useForm.tsx
+++ src/Hooks/useForm.tsx
@@ -2,6 +2,33 @@ import { useState } from "react";
 import { calcDelay } from "../Helpers/Helpers";
 import { MIN_ARRLENGTH } from "../Helpers/Config";
 
+const handleNumber = (e: number, opt?: string) => {
+  let name: string;
+  let value: number;
+  switch (opt) {
+    case "speed": {
+      name = "delay";
+      value = calcDelay(e);
+      return {
+        name: name,
+        value: value,
+      }
+    }
+    case "length": {
+      name = "arrLength";
+      value = e;
+      if (value <= MIN_ARRLENGTH) value = MIN_ARRLENGTH;
+      return {
+        name: name,
+        value: value,
+      }
+    }
+    default: {
+      throw  new Error('Number used without option');
+    }
+  }
+}
+
 export const useForm = (initialValues: any) => {
   const [values, setValues] = useState(initialValues);
   return [
@@ -10,22 +37,9 @@ export const useForm = (initialValues: any) => {
       let name: string;
       let value: string | number;
       if (typeof e === "number") {
-        switch (opt) {
-          case "speed": {
-            name = "delay";
-            value = calcDelay(e);
-            break;
-          }
-          case "length": {
-            name = "arrLength";
-            value = e;
-            if (value <= MIN_ARRLENGTH) value = MIN_ARRLENGTH;
-            break;
-          }
-          default: {
-            return;
-          }
-        }
+        const nameValueObj = handleNumber(e, opt);
+        name = nameValueObj.name;
+        value = nameValueObj.value;
       } else {
         name = e.target.name;
         value = e.target.value;
