3bd2a047612c833eb6d5604f1fd198ba8c469d1e
tommyrharper <thomasrobertharper@gmail.com>
2021-03-17 02:51:41 +0000

    Add selectionSort

--- src/Algos/pureAlgos.ts
+++ src/Algos/pureAlgos.ts
@@ -29,39 +29,24 @@ export const insertionSort = (array: number[]): number[] => {
     }
   }
   return arr;
-}
+};
 
-// let insertionSort = (array) => {
-//   const arr = [...array];
-//   for (let i = 1; i < arr.length; i++) {
-//     let j = i;
-//     while (j > 0 && arr[j - 1] > arr[j]) {
-//       const cache = arr[j];
-//       arr[j] = arr[j - 1];
-//       arr[j - 1] = cache;
-//       j--;
-//     }
-//   }
-//   return arr;
-// }
-
-// export const getInsertionSortAnimations = (array: number[]) => {
-//   // if (array.length <= 1) return [1];
-//   // const animations = [];
-//   const arr = [...array];
-//   // for (let i = 1; i < arr.length; i++) {
-//   //   let j = i;
-//   //   animations.push({
-
-//   //   })
-//   //   while (j > 0 && arr[j - 1] > arr[j]) {
-//   //     const cache = arr[j];
-//   //     arr[j] = arr[j - 1];
-//   //     arr[j - 1] = cache;
-//   //     j--;
-//   //   }
-//   // }
-//   return arr;
-// }
-
-// export const insertionSort = () => 4;
+export const selectionSort = (array: number[]): number[] => {
+  const arr = [...array];
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min = arr[i];
+    let minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j];
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      const cache = arr[i];
+      arr[i] = min;
+      arr[minIndex] = cache;
+    }
+  }
+  return arr;
+};
---
+++ src/Algos/selectionSort.ts
@@ -0,0 +1,37 @@
+import { Animation } from "../Helpers/Interfaces";
+
+export const getSelectionSortAnimations = (array: number[]): Animation[] => {
+  const arr = [...array];
+  const animations = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min = arr[i];
+    let minIndex = i;
+    const animation: Animation = {
+      positions: [i, i + 1],
+      endValues: [arr[i], arr[i + 1]],
+    };
+    animations.push(animation);
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < min) {
+        min = arr[j];
+        minIndex = j;
+      }
+      const animation: Animation = {
+        positions: [i, j, minIndex],
+        endValues: [arr[i], arr[j], arr[minIndex]],
+      };
+      animations.push(animation);
+    }
+    if (minIndex !== i) {
+      const cache = arr[i];
+      arr[i] = min;
+      arr[minIndex] = cache;
+      const animation: Animation = {
+        positions: [i, minIndex],
+        endValues: [arr[i], arr[minIndex]],
+      };
+      animations.push(animation);
+    }
+  }
+  return animations;
+};
