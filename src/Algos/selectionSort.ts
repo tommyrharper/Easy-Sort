a22c2433da3aa789ccd924bd20b56b8408ad5dc6
tommyrharper <thomasrobertharper@gmail.com>
2021-03-20 18:14:26 +0000

    Add colors to selectionSort

--- src/Algos/selectionSort.ts
+++ src/Algos/selectionSort.ts
@@ -9,6 +9,7 @@ export const getSelectionSortAnimations = (array: number[]): Animation[] => {
     const animation: Animation = {
       positions: [i, i + 1],
       endValues: [arr[i], arr[i + 1]],
+      colors: ['red', 'red']
     };
     animations.push(animation);
     for (let j = i + 1; j < arr.length; j++) {
@@ -19,6 +20,7 @@ export const getSelectionSortAnimations = (array: number[]): Animation[] => {
       const animation: Animation = {
         positions: [i, j, minIndex],
         endValues: [arr[i], arr[j], arr[minIndex]],
+      colors: ['red', 'yellow', 'red']
       };
       animations.push(animation);
     }
@@ -29,6 +31,7 @@ export const getSelectionSortAnimations = (array: number[]): Animation[] => {
       const animation: Animation = {
         positions: [i, minIndex],
         endValues: [arr[i], arr[minIndex]],
+        colors: ['red', 'red', 'red']
       };
       animations.push(animation);
     }
