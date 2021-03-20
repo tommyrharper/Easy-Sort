6b9523393a3359871f2026d2b05b63961284db85
tommyrharper <thomasrobertharper@gmail.com>
2021-03-20 18:16:13 +0000

    Add Quick Sort to algo information

--- src/Algos/AlgoInformation.tsx
+++ src/Algos/AlgoInformation.tsx
@@ -61,6 +61,21 @@ export const AlgoInformation: React.FC<{ algo: string }> = ({ algo }) => {
           Worst case: O(n)
         </Text>
       );
+    case "Quick Sort":
+      return (
+        <Text className="algoInfoContainer">
+          <b>Time complexity:</b>
+          <br />
+          Best case: O(n log(n))
+          <br />
+          Worst case: O(n<sup>2</sup>)<br />
+          Average: O(n log(n))<br />
+          <br />
+          <b>Space complexity:</b>
+          <br />
+          Worst case: O(n log(n))
+        </Text>
+      );
     default:
       return <noscript />;
   }
