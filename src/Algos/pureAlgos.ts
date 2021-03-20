070ba1efc62291d065d8809bfd438cbd11f22031
tommyrharper <thomasrobertharper@gmail.com>
2021-03-20 17:20:58 +0000

    Add quickSort to pureAlgos.ts

--- src/Algos/pureAlgos.ts
+++ src/Algos/pureAlgos.ts
@@ -51,23 +51,79 @@ export const selectionSort = (array: number[]): number[] => {
   return arr;
 };
 
-export const mergeSort = (array: number[]): any => {
-  const arr = [...array];
-  if (arr.length < 2) return arr;
+export const mergeSort = (array: number[]): number[] => {
+  const mergeSort = (array: number[]): any => {
+    const arr = [...array];
+    if (arr.length < 2) return arr;
 
-  const mid = Math.floor(arr.length / 2);
+    const mid = Math.floor(arr.length / 2);
 
-  const left = arr;
-  const right = arr.splice(mid);
+    const left = arr;
+    const right = arr.splice(mid);
 
-  return merge(mergeSort(left), mergeSort(right));
-}
+    return merge(mergeSort(left), mergeSort(right));
+  };
 
-const merge = (left: number[], right: number[]): (number | undefined)[] => {
-  let arr = [];
-  while (left.length && right.length) {
-    if (left[0] < right[0]) arr.push(left.shift())
-    else arr.push(right.shift());
-  }
-  return [...arr, ...left, ...right];
-}
+  const merge = (left: number[], right: number[]): (number | undefined)[] => {
+    let arr = [];
+    while (left.length && right.length) {
+      if (left[0] < right[0]) arr.push(left.shift());
+      else arr.push(right.shift());
+    }
+    return [...arr, ...left, ...right];
+  };
+  return mergeSort(array);
+};
+
+export const quickSort = (array: number[]) => {
+  const swap = (
+    array: number[],
+    leftIndex: number,
+    rightIndex: number
+  ): void => {
+    var temp = array[leftIndex];
+    array[leftIndex] = array[rightIndex];
+    array[rightIndex] = temp;
+  };
+
+  const partition = (array: number[], left: number, right: number): number => {
+    var pivot = array[Math.floor((right + left) / 2)], //middle element
+      i = left, //left pointer
+      j = right; //right pointer
+    while (i <= j) {
+      while (array[i] < pivot) {
+        i++;
+      }
+      while (array[j] > pivot) {
+        j--;
+      }
+      if (i <= j) {
+        swap(array, i, j); //sawpping two elements
+        i++;
+        j--;
+      }
+    }
+    return i;
+  };
+
+  const quickSort = (
+    array: number[],
+    left: number,
+    right: number
+  ): number[] => {
+    var index;
+    if (array.length > 1) {
+      index = partition(array, left, right); //index returned from partition
+      if (left < index - 1) {
+        //more elements on the left side of the pivot
+        quickSort(array, left, index - 1);
+      }
+      if (index < right) {
+        //more elements on the right side of the pivot
+        quickSort(array, index, right);
+      }
+    }
+    return array;
+  };
+  return quickSort(array, 0, array.length - 1);
+};
