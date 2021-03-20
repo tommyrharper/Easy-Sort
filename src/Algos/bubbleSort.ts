e940b3c21e35e1af9864a3f7d0b708968b255896
tommyrharper <thomasrobertharper@gmail.com>
2021-03-20 18:13:31 +0000

    Add yellow color to bubbleSort

--- src/Algos/bubbleSort.ts
+++ src/Algos/bubbleSort.ts
@@ -9,7 +9,9 @@ export const getBubbleSortAnimations = (array: number[]): Animation[] => {
     for (let i = 0; i < arr.length - 1; i++) {
       const bar1 = arr[i];
       const bar2 = arr[i + 1];
+      let barsSwitched = false;
       if (bar1 > bar2) {
+        barsSwitched = true;
         arr[i] = bar2;
         arr[i + 1] = bar1;
         changeMade = true;
@@ -17,6 +19,7 @@ export const getBubbleSortAnimations = (array: number[]): Animation[] => {
       const animation: Animation = {
         endValues: [arr[i], arr[i + 1]],
         positions: [i, i + 1],
+        colors: barsSwitched ? ['red', 'red'] : ['yellow', 'yellow']
       };
       animations.push(animation);
     }
