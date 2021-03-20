36767f77ff81db16cbefeabd70108aa0403f3e56
tommyrharper <thomasrobertharper@gmail.com>
2021-03-20 18:13:17 +0000

    Add colours to aglos.ts

--- src/Algos/algos.ts
+++ src/Algos/algos.ts
@@ -2,12 +2,14 @@ import { getBubbleSortAnimations } from "./bubbleSort";
 import { getInsertionSortAnimations } from "./insertionSort";
 import { getSelectionSortAnimations } from "./selectionSort";
 import { getMergeSortAnimations } from "./mergeSort";
+import { getQuickSortAnimations } from "./quickSort";
 import { Animation } from "../Helpers/Interfaces";
 interface AnimationFuncs {
   "Bubble Sort": (array: number[]) => Animation[];
   "Insertion Sort": (array: number[]) => Animation[];
   "Selection Sort": (array: number[]) => Animation[];
   "Merge Sort": (array: number[]) => Animation[];
+  "Quick Sort": (array: number[]) => Animation[];
 }
 
 export const animationFuncs: AnimationFuncs = {
@@ -15,6 +17,7 @@ export const animationFuncs: AnimationFuncs = {
   "Insertion Sort": getInsertionSortAnimations,
   "Selection Sort": getSelectionSortAnimations,
   "Merge Sort": getMergeSortAnimations,
+  "Quick Sort": getQuickSortAnimations,
 };
 
 export const executeAnimation = (
@@ -33,10 +36,13 @@ export const executeAnimation = (
     "bar"
   ) as HTMLCollectionOf<HTMLElement>;
   for (let i = 0; i < animations.length; i++) {
-    const { endValues, positions } = animations[i];
+    const { endValues, positions, colors } = animations[i];
     const pos1 = positions[0];
     const pos2 = positions[1];
     const pos3 = positions[2];
+    const color1 = colors[0];
+    const color2 = colors[1];
+    const color3 = colors[2];
     const height1 = `${calcHeight(endValues[0])}px`;
     const height2 = `${calcHeight(endValues[1])}px`;
     timeouts.push(setTimeout(() => {
@@ -45,9 +51,9 @@ export const executeAnimation = (
           arrayBars[j].style.backgroundColor = teal;
         }
       }
-      arrayBars[pos1].style.backgroundColor = "red";
-      arrayBars[pos2].style.backgroundColor = "red";
-      if (pos3) arrayBars[pos3].style.backgroundColor = "red";
+      arrayBars[pos1].style.backgroundColor = color1;
+      arrayBars[pos2].style.backgroundColor = color2;
+      if (pos3) arrayBars[pos3].style.backgroundColor = color3;
       timeouts.push(setTimeout(() => {
         arrayBars[pos1].style.height = height1;
         arrayBars[pos2].style.height = height2;
